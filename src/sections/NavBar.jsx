da0f5750b96f59ca727cbadbae6280f8e1e4b178
Hung Diep <hung@sampleassist.com>
2025-11-26 22:11:26 +0700

    Done UI NavBar

--- src/sections/NavBar.jsx
+++ src/sections/NavBar.jsx
@@ -4,28 +4,33 @@ import { NavBarItems } from "../contains";
 import BalanceCard from "../components/BalanceCard";
 const NavBar = () => {
   return (
-    <div className="mt-8 flex justify-between items-center">
-      {/* Logo */}
+    <section id="navbar">
+      <div className="mt-8 flex justify-between items-center">
+        {/* Logo */}
 
-      <div className=" gap-2 inline-flex items-center">
-        <img src={logo} alt="Logo" className="w-5 h-5" />
-        <p className="text-xl font-bold">H-IT</p>
+        <div className=" gap-2 inline-flex items-center">
+          <img src={logo} alt="Logo" className="w-5 h-5" />
+          <p className="text-xl font-bold">H-IT</p>
+        </div>
+
+        {/* NavBar */}
+        <ul className="inline-flex gap-10 items-center">
+          {NavBarItems.map((item) => {
+            return (
+              <li
+                key={item}
+                className="text-lg text-text-2 hover:text-white  cursor-pointer transition-all"
+              >
+                {item}
+              </li>
+            );
+          })}
+        </ul>
+
+        {/* ETH Balance + Address */}
+        <BalanceCard />
       </div>
-
-      {/* NavBar */}
-      <ul className="inline-flex gap-10 items-center">
-        {NavBarItems.map((item) => {
-          return (
-            <li key={item} className="text-lg">
-              {item}
-            </li>
-          );
-        })}
-      </ul>
-
-      {/* ETH Balance + Address */}
-      <BalanceCard />
-    </div>
+    </section>
   );
 };
 
